f8991074fd20355b1fc71845e84434bacb16e8ae
Victor <victor.blot@gmail.com>
2026-09-09 17:15:27 +0200

    feat: add link replacement instead of github 404 error

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './i18n';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 import MainLayout from './layouts/main/MainLayout.jsx';
 import ScrollUpdate from './layouts/main/ScrollUpdate.jsx';
@@ -11,7 +11,7 @@ import Project from './pages/project/Project.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <ScrollUpdate />
             <Routes>
                 <Route path="/" element={<MainLayout />}>
@@ -19,6 +19,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
                     <Route path="/project/:slug" element={<Project />} />
                 </Route>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>
 );
